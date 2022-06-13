8bac759b5a3d01e1ab45e8ccd7911b077df31526
Gagan <g.deepsingh1@gmail.com>
2022-06-13 17:12:18 -0600

    app install service workers

--- course-project-starting-setup/public/src/js/app.js
+++ course-project-starting-setup/public/src/js/app.js
@@ -1,5 +1,14 @@
+var deferredEvent;
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js", { source: "." }).then(() => {
     console.log("Service worker registered");
   });
 }
+
+window.addEventListener("beforeinstallprompt", (event) => {
+    console.log("before install prompt fired in the sites");
+    event.preventDefault();
+    deferredEvent = event;
+    return false;
+});
