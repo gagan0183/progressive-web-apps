728fc728b0dc691ff4242d1cfd30d085aa1c86c0
Gagan <g.deepsingh1@gmail.com>
2022-06-30 09:08:01 -0600

    app

--- course-project-starting-setup/public/src/js/feed.js
+++ course-project-starting-setup/public/src/js/feed.js
@@ -64,7 +64,6 @@ function createCard(data) {
   cardTitle.className = "mdl-card__title";
   cardTitle.style.backgroundImage = `url(${data.image})`;
   cardTitle.style.backgroundSize = "cover";
-  cardTitle.style.height = "180px";
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement("h2");
   cardTitleTextElement.className = "mdl-card__title-text";
