fdcfd0472326f6a2103ea0e620188eb5fd8df0ba
Gagan <g.deepsingh1@gmail.com>
2022-07-19 08:41:01 -0600

    app

--- course-project-starting-setup/public/sw-base.js
+++ course-project-starting-setup/public/sw-base.js
@@ -75,3 +75,85 @@ workboxSW.router.registerRoute(
 );
 
 workboxSW.precache([]);
+
+self.addEventListener("sync", function (event) {
+  console.log("[Service Worker] Background syncing", event);
+  if (event.tag === "sync-new-posts") {
+    console.log("[Service Worker] Syncing new posts");
+    event.waitUntil(
+      readData("sync-posts").then(function (data) {
+        for (var dt of data) {
+          var postData = new FormData();
+          postData.append("id", dt.id);
+          postData.append("title", dt.title);
+          postData.append("location", dt.location);
+          postData.append("file", dt.picture, dt.id + ".png");
+
+          fetch(
+            "https://us-central1-learnpwa-ee647.cloudfunctions.net/storePostData",
+            {
+              method: "POST",
+              body: postData,
+            }
+          )
+            .then(function (res) {
+              console.log("Sent data", res);
+              if (res.ok) {
+                res.json().then(function (response) {
+                  clearItemById("sync-posts", response.id);
+                });
+              }
+            })
+            .catch(function (err) {
+              console.log("Error while sending data", err);
+            });
+        }
+      })
+    );
+  }
+});
+
+self.addEventListener("notificationclick", function (event) {
+  var notification = event.notification;
+  var action = event.action;
+
+  console.log(notification);
+  if (action === "confirm") {
+    console.log("confirm");
+    notification.close();
+  } else {
+    console.log(action);
+    event.waitUntil(
+      clients.matchAll().then(function (cs) {
+        var client = cs.find(function (c) {
+          return c.visibilityState == "visible";
+        });
+        if (client) {
+          client.navigate("http://localhost:8080");
+          client.focus();
+        } else {
+          cs.openWindow("http://localhost:8080");
+        }
+      })
+    );
+    notification.close();
+  }
+});
+
+self.addEventListener("notificationclose", function (event) {
+  console.log("notification was closed", event);
+});
+
+self.addEventListener("push", function (event) {
+  console.log("Push notification received", event);
+  var data = { title: "New", content: "Contents" };
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+  var options = {
+    body: data.content,
+    icon: "/src/images/icons/app-icon-96x96.png",
+  };
+
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
