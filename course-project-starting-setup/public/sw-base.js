390723fe32edb2215168942b3e2a03410777d0d1
Gagan <g.deepsingh1@gmail.com>
2022-07-19 08:24:31 -0600

    offline html changes app

--- course-project-starting-setup/public/sw-base.js
+++ course-project-starting-setup/public/sw-base.js
@@ -4,13 +4,16 @@ importScripts("/src/js/utility.js");
 
 const workboxSW = new self.WorkboxSW();
 
-workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+workboxSW.router.registerRoute(
+  /.*(?:googleapis|gstatic)\.com.*$/,
+  workboxSW.strategies.staleWhileRevalidate({
     cacheName: "google-fonts",
     cacheExpiration: {
-        maxEntries: 3,
-        maxAgeSeconds: 60 * 60 * 24 * 30
-    }
-}));
+      maxEntries: 3,
+      maxAgeSeconds: 60 * 60 * 24 * 30,
+    },
+  })
+);
 
 workboxSW.router.registerRoute(
   "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
@@ -28,7 +31,7 @@ workboxSW.router.registerRoute(
 
 workboxSW.router.registerRoute(
   "https://learnpwa-ee647-default-rtdb.firebaseio.com/posts.json",
-  function(args) {
+  function (args) {
     return fetch(args.event.request).then(function (res) {
       var clonedRes = res.clone();
       clearData("posts")
@@ -41,6 +44,32 @@ workboxSW.router.registerRoute(
           }
         });
       return res;
+    });
+  }
+);
+
+workboxSW.router.registerRoute(
+  function (routeData) {
+    return routeData.event.request.headers.get("accept").includes("text/html");
+  },
+  function (args) {
+    return caches.match(args.event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(args.event.request)
+          .then(function (res) {
+            return caches.open("dynamic").then(function (cache) {
+              cache.put(args.event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch(function (err) {
+            return caches.match("/offline.html").then(function (res) {
+              return res;
+            });
+          });
+      }
     });
   }
 );
